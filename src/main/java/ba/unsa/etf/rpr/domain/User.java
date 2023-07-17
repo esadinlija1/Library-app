fdcc226699a1aef8eb7d3cd84588ba9ee5ce134f
esadinlija1 <esadinlija1@etf.unsa.ba>
2023-07-17 18:38:23 +0200

    Implemented getters and setters for User properties

--- src/main/java/ba/unsa/etf/rpr/domain/User.java
+++ src/main/java/ba/unsa/etf/rpr/domain/User.java
@@ -8,11 +8,36 @@ public class User implements IDable{
     private String phone; //For now, phone number will be String, maybe it will be changed to something else later
     @Override
     public void setId(int id) {
-
+         this.id=id;
     }
 
     @Override
     public int getId() {
-        return 0;
+        return this.id;
     }
+
+    public void setName(String name) {
+        this.name=name;
+    }
+    public void setEmail(String email) {
+        this.email=email;
+    }
+    public void setPhone(String phone) {
+        this.phone=phone;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getPhone(){
+        return this.phone;
+    }
+
+
+
 }
