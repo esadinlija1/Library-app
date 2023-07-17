d5992ef059d5b52547dac3187b1b739affdd0472
esadinlija1 <esadinlija1@etf.unsa.ba>
2023-07-17 18:28:00 +0200

    Implemented hashcode method for Book

--- src/main/java/ba/unsa/etf/rpr/domain/Book.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Book.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
+
 public class Book implements IDable{
 
     private int id;
@@ -34,6 +36,7 @@ public class Book implements IDable{
     }
 
 
+    @Override
     public String toString(){
         return "Book{" +
                 "id=" + id +
@@ -43,11 +46,17 @@ public class Book implements IDable{
     } //For now, I can't figure out when exactly I will need toString method, so for beginnig, it should return something
     //similar to a row in table
 
+    @Override
     public boolean equals(Object o){
         if(this==o) return true; //In case that the same instance on which method was called is passed as argument
         if(o==null || o.getClass()!=getClass()) return false; //In case that null-object or object that is not of Book type is passed as argument
         Book book=(Book) o; // o downcasted as Book
         return this.id==book.id; //check if id of current object and o are equal
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hashCode(id);
     }
 
 
