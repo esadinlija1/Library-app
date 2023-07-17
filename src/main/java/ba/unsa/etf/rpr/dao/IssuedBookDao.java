9d3b8e622fda2693835c38921f6a46f6a9965d8d
esadinlija1 <esadinlija1@etf.unsa.ba>
2023-07-17 21:53:14 +0200

    Added IssuedBookDao interface to dao package

---
+++ src/main/java/ba/unsa/etf/rpr/dao/IssuedBookDao.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.IssuedBook;
+
+
+/***
+ * Dao interface for IssuedBook domain bean
+ */
+public interface IssuedBookDao extends Dao<IssuedBook> {
+}
