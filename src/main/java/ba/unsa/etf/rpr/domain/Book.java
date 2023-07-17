da57830af03909c64060e22893a693c1d3703b50
esadinlija1 <esadinlija1@etf.unsa.ba>
2023-07-17 17:51:32 +0200

    Defined athributes for Book pojo

--- src/main/java/ba/unsa/etf/rpr/domain/Book.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Book.java
@@ -1,4 +1,20 @@
 package ba.unsa.etf.rpr.domain;
 
-public class Book {
+public class Book implements IDable{
+
+    private int id;
+    private String title;
+    private String author;
+
+    @Override
+    public void setId(int id) {
+
+    }
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+
 }
