7e5b956abbe97801bf4a848123746fbe277ff32b
esadinlija1 <esadinlija1@etf.unsa.ba>
2023-07-20 18:03:35 +0200

    Implemented DaoFactory class and completed dao refactoring

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -5,5 +5,24 @@ package ba.unsa.etf.rpr.dao;
  */
 public class DaoFactory {
 
+    private static final BookDao bookDao=new BookDaoSQLImpl();
+    private static final UserDao userDao=new UserDaoSQLImpl();
+    private static final IssuedBookDao issuedBookDao=new IssuedBookDaoSQLImpl();
+
+    private DaoFactory(){
+
+    }
+
+    public static BookDao bookDao(){
+        return bookDao;
+    }
+    public static UserDao userDao(){
+        return userDao;
+    }
+    public static IssuedBookDao issuedBooksDao(){
+        return issuedBookDao;
+    }
+
+
 
 }
