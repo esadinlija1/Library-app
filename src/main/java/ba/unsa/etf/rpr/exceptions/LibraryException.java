3eb82ef5b948666219f70828085dba42753ba1a7
esadinlija1 <esadinlija1@etf.unsa.ba>
2023-07-21 16:27:15 +0200

    Implemented two different constructors for LibraryException

--- src/main/java/ba/unsa/etf/rpr/exceptions/LibraryException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/LibraryException.java
@@ -1,4 +1,11 @@
 package ba.unsa.etf.rpr.exceptions;
 
-public class LibraryException {
+public class LibraryException extends Exception{
+    public LibraryException(String msg, Exception reason){
+        super(msg, reason);
+    }
+
+    public LibraryException(String msg){
+        super(msg);
+    }
 }
