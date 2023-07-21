9df3c3a7a2b58bf820cc83eafdb97d5432dc4cf6
esadinlija1 <esadinlija1@etf.unsa.ba>
2023-07-21 16:42:34 +0200

    Implemented method for validation of title field in BookManager

--- src/main/java/ba/unsa/etf/rpr/business/BookManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/BookManager.java
@@ -1,4 +1,21 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.domain.Book;
+import ba.unsa.etf.rpr.exceptions.LibraryException;
+
+/***
+ * Business logic layer for management of Books
+ */
+
 public class BookManager {
+
+    /***
+     * Following method is used to validate title field when adding new book, or making an update
+     * @param title
+     */
+    public void validateBookTitle(String title) throws LibraryException {
+        if(title==null || title.length()<3 || title.length()>100)
+            throw new LibraryException("Title of the book is too short");
+    }
+
 }
