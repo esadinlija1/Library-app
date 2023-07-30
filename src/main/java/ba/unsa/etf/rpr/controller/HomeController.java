095891dcdad485217f530522bc0103840be76fd4
esadinlija1 <esadinlija1@etf.unsa.ba>
2023-07-30 11:49:32 +0200

    Implemented method for opening of Books tab and added method for opening new tabs

--- src/main/java/ba/unsa/etf/rpr/controller/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/HomeController.java
@@ -1,6 +1,13 @@
 package ba.unsa.etf.rpr.controller;
 
+import java.awt.event.ActionEvent;
+
 public class HomeController {
 
+public void openBooksTab(ActionEvent event){
+    openNewTab("/fxml/books.fxml",new BooksController());
+}
 
+    private void openNewTab(String file,Object con) {
+    }
 }
