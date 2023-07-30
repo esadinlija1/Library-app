b737d07c6362eb6c076c4869149d18e7256bb6b7
esadinlija1 <esadinlija1@etf.unsa.ba>
2023-07-30 11:53:45 +0200

    Implemented method for opening new tabs in home dashboard

--- src/main/java/ba/unsa/etf/rpr/controller/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/HomeController.java
@@ -1,6 +1,15 @@
 package ba.unsa.etf.rpr.controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class HomeController {
 
@@ -8,6 +17,16 @@ public void openBooksTab(ActionEvent event){
     openNewTab("/fxml/books.fxml",new BooksController());
 }
 
-    private void openNewTab(String file,Object con) {
+    private void openNewTab(String file,Object controller) {
+        try{
+            FXMLLoader loader=new FXMLLoader(getClass().getResource(file));
+            loader.setController(controller);
+            Stage stage=new Stage();
+            stage.setScene(new Scene(loader.<Parent>load(),USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.initStyle(StageStyle.UTILITY);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
