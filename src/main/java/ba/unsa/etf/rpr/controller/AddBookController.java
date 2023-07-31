6275222a8f24d52e447cfaa0c8c4942af1addc0d
esadinlija1 <esadinlija1@etf.unsa.ba>
2023-07-31 12:49:39 +0200

    Implemented controller class for addbook.fxml

--- src/main/java/ba/unsa/etf/rpr/controller/AddBookController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/AddBookController.java
@@ -1,8 +1,55 @@
 package ba.unsa.etf.rpr.controller;
 
 
+import ba.unsa.etf.rpr.business.BookManager;
+import ba.unsa.etf.rpr.domain.Book;
+import ba.unsa.etf.rpr.exceptions.LibraryException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 /***
  * Book adding form controller class
  */
 public class AddBookController {
+
+    private final BookManager bookManager=new BookManager();
+
+    @FXML
+    private TextField titleField=new TextField();
+
+    @FXML
+    private TextField authorField=new TextField();
+
+    @FXML
+    private Button addBookButton=new Button();
+
+    private void addBook() throws LibraryException {
+        bookManager.add(new Book(0,titleField.getText(),authorField.getText()));
+        backToBooksTab();
+    }
+
+    private void backToBooksTab(){
+        try{
+            FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/books.fxml"));
+            //loader.setController(controller);
+            Stage stage=new Stage();
+            stage.setScene(new Scene(loader.<Parent>load(),USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.initStyle(StageStyle.UTILITY);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
