a406c29b539a702b5250087a1ce673d8de7b8852
esadinlija1 <esadinlija1@etf.unsa.ba>
2023-09-03 21:12:13 +0200

    Implemented initialize method for UserController class

--- src/main/java/ba/unsa/etf/rpr/controller/UsersController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/UsersController.java
@@ -1,18 +1,20 @@
 package ba.unsa.etf.rpr.controller;
 
-import ba.unsa.etf.rpr.business.BookManager;
 import ba.unsa.etf.rpr.business.UserManager;
-import ba.unsa.etf.rpr.domain.Book;
+import ba.unsa.etf.rpr.controller.components.ActionCellFactory;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.LibraryException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
+import java.net.URL;
 import java.util.List;
 import java.util.Optional;
+import java.util.ResourceBundle;
 
 
 /***
@@ -64,6 +66,29 @@ public class UsersController {
     private ObservableList<User> usersObservableList= FXCollections.observableArrayList();
 
 
+    @FXML
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        idColumn.setCellValueFactory(new PropertyValueFactory<User, Integer>("id"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<User, String>("name"));
+        emailColumn.setCellValueFactory(new PropertyValueFactory<User, String>("email"));
+        phoneColumn.setCellValueFactory(new PropertyValueFactory<User,String>("phone"));
+        actionColumn.setCellValueFactory(new PropertyValueFactory<User,Integer>("id"));
+
+        //Implementing search feature-on click of button, the search method in manager will be called and list in
+        //table will be updated
+
+        actionColumn.setCellFactory(new ActionCellFactory(editEvent -> {
+            int userId = Integer.parseInt(((Button)editEvent.getSource()).getUserData().toString());
+            updateUser(userId);
+        }, (deleteEvent -> {
+            int userId = Integer.parseInt(((Button)deleteEvent.getSource()).getUserData().toString());
+            deleteUser(userId);
+        })));
+        usersTable.setItems(FXCollections.observableList(userManager.getAll()));
+        usersTable.refresh();
+
+    }
     /***
      * Method that is used when initializing to get list of all users, but also when we do any kind of change in list, i.e.
      * delete a user or update data of current user
@@ -85,7 +110,7 @@ public class UsersController {
         refreshUsers(userManager.searchByName(name));
     }
 
-    public void updateUsers(int userId){
+    public void updateUser(int userId){
 
     }
 
