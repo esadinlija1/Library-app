f4614491f9d2a0fd2741e2a5e93b9287d1381596
esadinlija1 <esadinlija1@etf.unsa.ba>
2023-09-04 11:04:58 +0200

    Implemented business logic for IssuedBook class

--- src/main/java/ba/unsa/etf/rpr/controller/IssuedBookController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/IssuedBookController.java
@@ -1,4 +1,23 @@
 package ba.unsa.etf.rpr.controller;
 
-public class IssuedBookController {
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/***
+ * Controller class for IssuedBooks tab
+ */
+public class IssuedBookController implements Initializable {
+
+    @FXML
+    public TableView issuedBooks=new TableView();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
